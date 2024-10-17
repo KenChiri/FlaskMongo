e86d344d9ef75cecb3e0484c9e321c7b75408d58
KenChiri <Kennykipc@gmail.com>
2024-10-18 00:08:49 +0300

    security clean

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ mongo = PyMongo(app)
 # Mail setup for password reset
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'kennykipc@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jfxf gvtb kric eegz'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
